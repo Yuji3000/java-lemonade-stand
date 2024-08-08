5a0a5c2b9b2d1f567bf6bc2972920307e827c3ac
Yuji Kosakowski <yuji.k.dev@gmail.com>
2024-08-08 12:52:29 -0600

    Created and add getters for lemonade.java

--- src/main/java/lemonadestand/model/Lemonade.java
+++ src/main/java/lemonadestand/model/Lemonade.java
@@ -2,4 +2,16 @@ package lemonadestand.model;
 
 public class Lemonade {
 
+	private double lemonJuice;
+	
+	private double water;
+	
+	private double sugar;
+	
+	private int iceCubes;
+	
+	private final int CUP = 1;
+	// final means this is a constant, name must be all uppercase
+	
+	private double price;
 }
